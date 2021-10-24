77f595d9454f88f8d3105cf8845828a97cec713b
Nibbee <niko.varjonen@edu.turkuamk.fi>
2021-10-24 21:30:38 +0300

    Created contact us form on about us page

--- validation.js
+++ validation.js
@@ -1,4 +1,36 @@
 function validation() {
 
-   alert("working");
+    var name = document.getElementById("name").value;
+    var subject = document.getElementById("subject").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message").value;
+
+    if (name.length < 5) {
+        text = "Please enter a valid name"
+        error_message.innerHTML = text;
+        return false;
+    }
+    if (subject.length < 10) {
+        text = "Please enter a valid subject";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if (isNaN(phone) || phone.length != 10) {
+        text = "Please enter a valid phonenumber";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if (email.indexOf("@") == -1 || email.length < 10) {
+        text = "Please enter a valid email";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if (message.length == "") {
+        text = "Please enter your message";
+        error_message.innerHTML = text;
+        return false;
+    }
+    return false;
 }
