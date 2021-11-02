4235e8fc59cad91b02f12a99fa9212a73af1c8f0
njiv <varjonenniko@outlook.com>
2021-11-02 10:46:38 +0200

    Removed testi.txt

--- js/validation.js
+++ js/validation.js
@@ -1,38 +1,39 @@
-function validation() {
-
+function validate() {
     var name = document.getElementById("name").value;
     var subject = document.getElementById("subject").value;
     var phone = document.getElementById("phone").value;
     var email = document.getElementById("email").value;
     var message = document.getElementById("message").value;
-    var error_message = document.getElementById("error_message").value;
+    var error_message = document.getElementById("error_message");
 
     error_message.style.padding = "10px";
 
+    var text;
     if (name.length < 5) {
-        text = "Please enter a valid name"
+        text = "Please Enter valid Name";
         error_message.innerHTML = text;
         return false;
     }
     if (subject.length < 10) {
-        text = "Please enter a valid subject";
+        text = "Please Enter Correct Subject";
         error_message.innerHTML = text;
         return false;
     }
     if (isNaN(phone) || phone.length != 10) {
-        text = "Please enter a valid phonenumber";
+        text = "Please Enter valid Phone Number";
         error_message.innerHTML = text;
         return false;
     }
-    if (email.indexOf("@") == -1 || email.length < 10) {
-        text = "Please enter a valid email";
+    if (email.indexOf("@") == -1 || email.length < 6) {
+        text = "Please Enter valid Email";
         error_message.innerHTML = text;
         return false;
     }
-    if (message.length == "") {
-        text = "Please enter your message";
+    if (message.length <= 140) {
+        text = "Please Enter More Than 140 Characters";
         error_message.innerHTML = text;
         return false;
     }
-    return false;
+    alert("Form Submitted Successfully!");
+    return true;
 }
